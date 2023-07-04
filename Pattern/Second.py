8aaa746befa93bcce11c311dd326c06efeafb3fb
CSTVann <108693098+CSTVann@users.noreply.github.com>
2023-07-04 15:25:34 +0700

    Create Second.py

---
+++ Pattern/Second.py
@@ -0,0 +1,4 @@
+for i in range (4):#first loop for row 
+    for j in range(4-i):#cuz we want to print the first row cuz i count from 0
+        print('* ', end='')
+    print()#print here for break line after the row loop run
